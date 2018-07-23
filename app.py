31e6c9369a18253f105395f55c081d5e91f290bf
grioelts <grioelts@gmail.com>
2018-07-23 15:28:39 +0200

    login structure (without db of users or passwordcheck)

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
+from helpers import login_required
 
 app = Flask(__name__)
-
+app.secret_key = b'3RT6HJ8L'
 @app.route("/")
+@login_required
 def test():
     return render_template("test.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        # check for complete form better in JS
+        if not request.form.get("username") or not request.form.get("password"):
+            return render_template("retry.html")
+        session["user_id"] = request.form.get("username")# need to change
+        return redirect("/")
