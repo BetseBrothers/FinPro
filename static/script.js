09ce41f3160a2e0b3a662b963b3a48c41acf0017
grioelts <grioelts@gmail.com>
2018-08-13 15:46:26 +0200

    rekening.html

--- static/script.js
+++ static/script.js
@@ -4,4 +4,18 @@ setInterval(function(){
     document.getElementById('timestamp').innerHTML = timestamp
 }, 1000);
 
-//document.getElementb().onsubmit = function(){
+function isEmpty(){
+    for (i = 0; i < document.getElementById("form").children.length - 1; i++)
+        if (document.getElementById("form").children[i].value == ""){
+            return true;
+        }
+    return false;
+}
+
+document.getElementById("form").onsubmit = function(){
+    if (isEmpty()){
+        alert("Need to fill in form")
+        return false;
+    }
+    else{return true}
+};
